5fd7094c6b2f793e1b49598d5d87c95f82617b5d
Dragan <milosevicdragan002@gmail.com>
2023-10-18 17:41:38 +0200

    Define inputs and output

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ data = pd.read_csv('ecoli.csv',header=None,sep= '\s+')
 col_names = ["sequence_name","mcg","gvh","lip","chg","aac","alm1","alm2","site"]
 data.columns = col_names
 
+X = data.iloc[:,1:8]
+Y = data['site']
+
