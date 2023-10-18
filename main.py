cf27a167561b9142ac7ecefef6ee7efc89bd2d08
Dragan <milosevicdragan002@gmail.com>
2023-10-18 17:40:43 +0200

    Define columns

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('ecoli.csv',header=None,sep= '\s+')
+col_names = ["sequence_name","mcg","gvh","lip","chg","aac","alm1","alm2","site"]
+data.columns = col_names
+
