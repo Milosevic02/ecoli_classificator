825d7c883de58028dd0f884385a62506be174a38
Dragan <milosevicdragan002@gmail.com>
2023-10-18 17:43:41 +0200

    train test split

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ col_names = ["sequence_name","mcg","gvh","lip","chg","aac","alm1","alm2","site"]
 data.columns = col_names
 
 X = data.iloc[:,1:8]
-Y = data['site']
+y = data['site']
 
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)
